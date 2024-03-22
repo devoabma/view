133319bf4cb4515f5fef7ef69a253b838b3538c7
Hilquias Ferreira Melo <hilquiasfmelo@hotmail.com>
2024-03-22 20:33:14 -0300

    feat: add link details

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,5 @@
+import { Info } from 'lucide-react'
+
 import { Separator } from '@/components/ui/separator'
 
 import CardImage from '../../assets/cardImage.jpeg'
@@ -17,14 +19,33 @@ export function Home() {
       </h1>
 
       <div className="mx-auto mt-12 grid w-80 place-items-center gap-x-10 md:w-[700px] md:grid-cols-2 md:px-10 lg:w-[950px] lg:grid-cols-3 lg:px-5">
-        {Array.from({ length: 6 }, (_, i) => {
+        {Array.from({ length: 5 }, (_, i) => {
           return (
-            <img
-              key={i}
-              src={CardImage}
-              alt="Card Image"
-              className="mb-10 h-96 w-full border shadow-custom"
-            />
+            <div key={i} className="mb-10 flex  flex-col gap-2">
+              <img
+                src={CardImage}
+                alt="Card Image"
+                className="h-96 w-full cursor-pointer border shadow-custom transition-all hover:-translate-y-2"
+              />
+
+              <strong className="text-base font-extrabold uppercase">
+                26 de Março de 2024
+              </strong>
+              <Separator orientation="horizontal" />
+              <div className="space-y-1">
+                <div className="flex items-center justify-between">
+                  <span className="text-muted-foreground">São Luís - MA</span>
+                  <Separator orientation="vertical" className="h-5" />
+                  <p className="flex cursor-pointer items-center gap-1 text-sm font-medium text-rose-700 hover:text-rose-900">
+                    <Info className="h-4 w-4" />
+                    Mais detalhes
+                  </p>
+                </div>
+                <p className="text-sm font-semibold tracking-tight">
+                  Audiência Pública sobre o Código Eleitoral em Previdenciário
+                </p>
+              </div>
+            </div>
           )
         })}
       </div>
